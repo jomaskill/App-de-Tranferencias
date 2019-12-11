aa551071f6c0d07ad4d0a7dec7d73269e8f309e9
jomas <jomasjoao@gmail.com>
2019-12-11 19:44:16 -0300

    Criando uma lista com Card e listTitle

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(
    MaterialApp(
      home: Scaffold(
+       body: Column(
+         children: <Widget>[
+           Card(
+             child: ListTile(
+               leading: Icon(Icons.monetization_on),
+               title: Text('100.00'),
+               subtitle: Text('34233-5'),
+             ),
+           ),
+           Card(
+             child: ListTile(
+               leading: Icon(Icons.monetization_on),
+               title: Text('100.00'),
+               subtitle: Text('34233-5'),
+             ),
+           ),
+         ],
+       ),
        appBar: AppBar(title: Text('Tranferências'),),
-         floatingActionButton: FloatingActionButton(
+       floatingActionButton: FloatingActionButton(
            child: Icon(Icons.add),
          ),
      ),
