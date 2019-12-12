c85212a0797f15a4544970e1dc15ecb388c2ae71
jomas <jomasjoao@gmail.com>
2019-12-11 23:59:41 -0300

    Refatoramento do código com a criação de classe

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
    MaterialApp(
      home: Scaffold(
-       body: Column(
-         children: <Widget>[
-           Card(
-             child: ListTile(
-               leading: Icon(Icons.monetization_on),
-               title: Text('100.00'),
-               subtitle: Text('34233-5'),
-             ),
-           ),
-           Card(
-             child: ListTile(
-               leading: Icon(Icons.monetization_on),
-               title: Text('100.00'),
-               subtitle: Text('34233-5'),
-             ),
-           ),
-         ],
-       ),
+       body: ListaTranferencias(),
        appBar: AppBar(title: Text('Tranferências'),),
        floatingActionButton: FloatingActionButton(
            child: Icon(Icons.add),
@@ -28,3 +11,47 @@ void main() => runApp(
      ),
    )
 );
+
+
+class ListaTranferencias extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[
+          ItemTranferencia(Transferencia(500.00, 'Juliana')),
+          ItemTranferencia(Transferencia(2000.00, 'Carlos')),
+          ItemTranferencia(Transferencia(3000.00, 'Juliana')),
+        ]
+    );
+  }
+}
+
+class ItemTranferencia extends StatelessWidget{
+
+  final Transferencia _transferencia;
+
+
+  ItemTranferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.conta.toString()),
+        subtitle: Text(_transferencia.valor.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+
+  final String conta;
+  final double valor;
+
+
+  Transferencia(this.valor, this.conta);
+
+}
